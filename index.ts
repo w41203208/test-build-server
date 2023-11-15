62f34cbea0b61da49d35c7d941813a6bdc4e5c62
Jay <w41203208@gmail.com>
2023-11-15 15:36:51 +0800

    first

---
+++ index.ts
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 8787;
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: any, res: any) => {
+  return res.send('Hellow');
+});
+
+const server = app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
