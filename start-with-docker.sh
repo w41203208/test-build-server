daaf342e0caedb5b73301679b1ebe27e0a8705ab
Jay <w41203208@gmail.com>
2023-11-16 13:16:15 +0800

    update start-with-docker.sh

--- start-with-docker.sh
+++ start-with-docker.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-sudo docker build -t test-server-jay:test .
+container_id=$(sudo docker ps -a | grep $1 | awk '{print $1}')
+
+if container_id != ""; then
+  sudo docker rm $container_id
+fi
+
+image_id=$(sudo docker image ls | grep $1 | awk '{print $3}')
+
+if image_id != ""; then
+  sudo docker rmi $image_id
+fi
+
+sudo docker build -t $1:test .
 sudo docker compose -f ./docker-compose.yml up -d
